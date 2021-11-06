e3daa7342a505e89953b427c4c14c12a6ea907e3
Tadesse Jemal <tadessejemal63@gmail.com>
2021-11-06 10:31:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Navbar , NavbarBrand } from "reactstrap";
+import Menu from './Components/MenuComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return( 
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">foods</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu /> 
+      </div>
+    );
+  }
 }
+
 
 export default App;
